2b57076ebddef1836669b0bb008c63e80ab717ae
topjz <chen3jian@vip.qq.com>
2020-05-11 18:54:11 +0800

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -1,12 +1,12 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// | Topjz
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://topjz.com All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: yunwuxin <448901948@qq.com>
+// | Author: chen3jian <chen3jian@gmail.com>
 // +----------------------------------------------------------------------
 
 if (!function_exists('throw_if')) {
