571a601ed18c4beaf0aa32fc3fe3ddc23ee2ee0b
topjz <chen3jian@vip.qq.com>
2020-05-11 19:31:41 +0800

    添加注释

--- src/Collection.php
+++ src/Collection.php
@@ -1,12 +1,12 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// | Topjz
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2019 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2016 http://topjz.com All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: zhangyajun <448901948@qq.com>
+// | Author: chen3jian <chen3jian@gmail.com>
 // +----------------------------------------------------------------------
 declare (strict_types = 1);
 
--- src/helper.php
+++ src/helper.php
@@ -8,7 +8,6 @@
 // +----------------------------------------------------------------------
 // | Author: chen3jian <chen3jian@gmail.com>
 // +----------------------------------------------------------------------
-
 if (!function_exists('throw_if')) {
     /**
      * 按条件抛异常
@@ -16,8 +15,7 @@ if (!function_exists('throw_if')) {
      * @param mixed            $condition
      * @param Throwable|string $exception
      * @param array            ...$parameters
-     * @return mixed
-     *
+     * @return mixed     *
      * @throws Throwable
      */
     function throw_if($condition, $exception, ...$parameters)
